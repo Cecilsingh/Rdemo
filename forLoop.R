7941e31adfaeaa15c334a6615f8ce6b9d7c6526b
Cecilsingh <54638818+Cecilsingh@users.noreply.github.com>
2024-08-08 12:53:52 +1000

    Create forLoop.R

---
+++ forLoop.R
@@ -0,0 +1,31 @@
+###Load Packages###
+
+#Load parallel package
+#Allows for parallel processing
+library(parallel)
+
+###Functions###
+
+#Create function that loops through 1000 times
+createForLoop <- function(i){
+  for (i in 1:1000){
+    j=i+1
+  }  
+}
+
+###Capture Results###
+
+#Non-parallel code execution
+#lapply iterates over the while loop
+#Runs the createLoop function above 1000 times
+system.time(nonParallelFunc <- lapply(1:1000, createForLoop))
+
+#Parallel code execution
+#mclapply is provided by the parallel package, and splits the iteration of multiple cores
+#Runs the createLoop function above 1000 times
+system.time(parallelFunc <- mclapply(1:1000, createForLoop))
+
+###Display Function Results###
+#createForLoop()
+
+
