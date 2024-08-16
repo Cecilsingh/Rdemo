d6af3ff62e6348efd3a84dd9faa4c3f34396a1b8
Cecilsingh <54638818+Cecilsingh@users.noreply.github.com>
2024-08-16 10:32:15 +1000

    Update memUsage.R

--- memUsage.R
+++ memUsage.R
@@ -7,14 +7,26 @@ firstHalfMem <- rep(1, 1e9)
 
 secondHalfMem <- rep(1, 1e9)
 
-additionalMem <- rep(1, 1e8)
+#additionalMem <- rep(1, 1e8)
 
+#Measures the time taken for a specific block of code to execute, in this case, a for loop
+#The tictoc package provides functions to measure time
+#tictoc::tic() starts the timer and records the current time which is used to measure the duration of code execution
+#The for loop iterates 100 times
+#tictoc::toc() stops the timer
+#tic() shows how long the code inside tic() and toc() calls took to execute
 library(tictoc)
 
 tictoc::tic()
 
-for (i in 1:1000){
+for (i in 1:100){
   j=i+1
+  gc()
 }
 
 tictoc::toc()
+
+
+rm(firstHalfMem)
+
+gc()
