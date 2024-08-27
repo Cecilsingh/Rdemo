b39970bf9817a01ee5b3688534f2c3ccc4935dac
Cecilsingh <54638818+Cecilsingh@users.noreply.github.com>
2024-08-27 14:41:37 +1000

    Update forLoop.R

--- cpuMgmt/forLoop.R
+++ cpuMgmt/forLoop.R
@@ -18,12 +18,12 @@ createForLoop <- function(i){
 #Non-parallel code execution
 #lapply iterates over the while loop
 #Runs the createLoop function above 1000 times
-system.time(nonParallelFunc <- lapply(1:1000, createForLoop))
+system.time(nonParallelFunc <- lapply(1:10000, createForLoop))
 
 #Parallel code execution
 #mclapply is provided by the parallel package, and splits the iteration of multiple cores
 #Runs the createLoop function above 1000 times
-system.time(parallelFunc <- mclapply(1:1000, createForLoop))
+system.time(parallelFunc <- mclapply(1:10000, createForLoop))
 
 ###Display Function Results###
 #createForLoop()
